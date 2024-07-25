eedfd050139a50aa980689e8b346396cafd9ea6f
Charles Dong <chardon_cs@proton.me>
2024-07-25 23:19:40 +0800

    Merge uses

--- src/webui.rs
+++ src/webui.rs
@@ -21,11 +21,10 @@ use std::collections::HashMap;
 use std::ffi::CStr;
 use std::ffi::CString;
 use std::os::raw::c_char;
-use std::sync::Mutex;
+use std::sync::{Mutex, MutexGuard};
 
 use bindgen::*;
 use once_cell::sync::Lazy;
-use std::sync::MutexGuard;
 
 // Consts
 pub const true_: u32 = 1;
